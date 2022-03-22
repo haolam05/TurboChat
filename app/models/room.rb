3974f9d10890ac6af493d6abf63ef32e0d2d6efe
Hao Lam <tuonghao2001@gmail.com>
2022-03-21 17:37:47 -0700

    test video deployment 2

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,9 @@
 class Room < ApplicationRecord
-  before_create do
-    opentok = OpenTok::OpenTok.new Rails.application.credentials.vonage_api_key, Rails.application.credentials.vonage_api_secret
-    session = opentok.create_session
-    self.vonage_session_id = session.session_id
-  end
+  # before_create do
+  #   opentok = OpenTok::OpenTok.new Rails.application.credentials.vonage_api_key, Rails.application.credentials.vonage_api_secret
+  #   session = opentok.create_session
+  #   self.vonage_session_id = session.session_id
+  # end
 
   after_initialize :assigned_vonage_session_id_to_existing_rooms
 
